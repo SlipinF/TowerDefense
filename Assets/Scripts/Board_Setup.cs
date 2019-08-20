d26697b1e628fa8608244aed3e88665f253cac60
SlipinF <itzamnass@gmail.com>
2019-08-20 22:07:00 +0200

    BoardSetup

---
+++ Assets/Scripts/Board_Setup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board_Setup : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tile;
+    GameObject Copy;
+
+    private void Start()
+    {
+        GameObject[,] TileMatrix = new GameObject[5,5]; // change here if you want to change the size of the board!
+        InstatiateTiles(TileMatrix);
+    }
+
+    void InstatiateTiles(GameObject[,] ArrayOfTiles)
+    {
+        int CurrentRow = 0;
+        int CurrentCol = 0;
+                
+        foreach (var cor in ArrayOfTiles)
+        {
+            Copy = Instantiate(tile, new Vector3(CurrentRow, 0, CurrentCol),Quaternion.identity.normalized,gameObject.transform);
+
+            CurrentCol++;
+
+            if (CurrentCol >= ArrayOfTiles.GetLength(0))
+            {
+                CurrentRow++;
+                CurrentCol = 0;
+            }
+            if (CurrentRow >= ArrayOfTiles.GetLength(1))
+            {
+                CurrentRow = 0;
+            }
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private void OnMouseOver()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.green;
+    }
+    private void OnMouseExit()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.white;
+    }
+}
