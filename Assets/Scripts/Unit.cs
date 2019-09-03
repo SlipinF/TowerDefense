f570a0ca98ceef8ee5507affd782d09d050651da
SlipinF <itzamnass@gmail.com>
2019-09-03 12:32:05 +0200

    NavMeshLogic

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Unit : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject finishPoint;
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {  
+            agent.SetDestination(finishPoint.transform.position);
+        }
+    }
+}
