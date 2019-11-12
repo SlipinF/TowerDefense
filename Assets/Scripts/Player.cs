9c66fb36fa0b073a180d0d99f1f7283e0f441482
Kacper Duda <kacper.d.duda@gmail.com>
2019-11-12 21:01:49 +0100

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,8 @@ public class Player : MonoBehaviour
     public static int gold = 100;
     public static int level = 1;
 
+
+
     public static int ChangeGold(int price, bool sell)
     {
         if(sell){
